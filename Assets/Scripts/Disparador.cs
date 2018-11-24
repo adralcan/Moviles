97c4722b1124c78691fbb54af645458429402519
mjcguada <mjcguada@gmail.com>
2018-11-24 14:52:30 +0100

    Puntero y recoleccion de bolas

--- Assets/Scripts/Disparador.cs
+++ Assets/Scripts/Disparador.cs
@@ -8,28 +8,54 @@ public class Disparador : MonoBehaviour {
     GameObject sprite;
     public int fuerza = 5;
 
-    int contBolas;
-
-	// Use this for initialization
-	void Start () {
-        //Leer nivel, este nos dice las bolas (?) o es el archivo de guardado?
-        contBolas = 5;		
-	}
-
+    public int contBolas = 5;
 
     Vector2 direccion;
+    LineRenderer lineRenderer;
+
+    public float grosorLinea = 0.2f;
+
+    Color c1 = Color.grey;
+    Color c2 = Color.black;
+
+    // Use this for initialization
+    void Start () {
+        //Propiedades de la linea-trayectoria
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        lineRenderer.widthMultiplier = grosorLinea;
+
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) }
+            );
+        lineRenderer.colorGradient = gradient;        
+    }
+           
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (Input.GetMouseButton(0))
+        {
+            //Dibujar linea
+            lineRenderer.SetPosition(0, new Vector3(transform.position.x, transform.position.y, 0));
+            lineRenderer.SetPosition(1, Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0)));
+        }
+
         //Version PC        
         if (Input.GetMouseButtonUp(0))
-        {            
+        {
+            //Borramos linea
+            lineRenderer.SetPosition(0, new Vector3(transform.position.x, transform.position.y, 0));
+            lineRenderer.SetPosition(1, new Vector3(transform.position.x, transform.position.y, 0));
+
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-            Debug.Log(mousePosition);
-            Debug.DrawLine(transform.position, (transform.position + mousePosition) * 10, Color.red, Mathf.Infinity);
+            //Debug.Log(mousePosition);
+            //Debug.DrawLine(transform.position, (transform.position + mousePosition) * 10, Color.red, Mathf.Infinity);
             
             direccion = mousePosition - transform.position;
-            direccion = direccion.normalized;            
+            direccion = direccion.normalized;  
             
             Disparar();                      
         }
@@ -41,18 +67,21 @@ public class Disparador : MonoBehaviour {
     }
 
     void Disparar()
-    {
-        TextMesh textAux = gameObject.GetComponent<TextMesh>();
-        textAux.text = contBolas.ToString();
+    {        
+        gameObject.GetComponent<TextMesh>().text = contBolas.ToString();
 
-        contBolas--;
-        GameObject aux = Instantiate(bolaPrefab);
-        //Nos aseguramos z = 0
-        aux.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-        //aux.GetComponent<Rigidbody2D>().AddForce(direccion * fuerza * Time.deltaTime);
-        aux.GetComponent<Rigidbody2D>().velocity = direccion * fuerza * Time.deltaTime;
-        if(contBolas > 0)
-            Invoke("Disparar", 0.1f);
+        if (contBolas > 0)
+        {
+            contBolas--;
+            GameObject aux = Instantiate(bolaPrefab);
+            //Nos aseguramos z = 0
+            aux.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+            //aux.GetComponent<Rigidbody2D>().AddForce(direccion * fuerza * Time.deltaTime);
+            aux.GetComponent<Rigidbody2D>().velocity = direccion * fuerza * Time.deltaTime;
+
+            if (contBolas > 0)
+                Invoke("Disparar", 0.1f);
+        }
     }
 
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,53 +5,134 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     public static GameManager instance;
 
     //Objetos escena
     public GameObject Disparador;
-    [HideInInspector] public Canvas Canvas;
+    public GameObject Recolector;
 
     //Prefabs
     public GameObject muro;
     public GameObject bloque;
 
-
     public float distanciaX = 0.2f;
     public float distanciaY = 0.2f;
+
+    struct infoBloque
+    {
+        public int x;
+        public int y;
+        public int tipo;
+    }
 
     // Use this for initialization
     void Start()
     {
         instance = this;
-        Canvas = new Canvas();
         colocarObjetos();
-        ReadLevel("mapdata0.txt");
-    }    
+        ReadLevel("mapdata1.txt");
+    }
 
     void colocarObjetos()
     {
-            Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.1f, Camera.main.nearClipPlane));
-            Disparador.transform.position = new Vector3(pos.x, pos.y, 0);
+        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.1f, Camera.main.nearClipPlane));
+        Disparador.transform.position = new Vector3(pos.x, pos.y, 0);
 
-            GameObject muroIzquierdo = Instantiate(muro);
-            pos = Camera.main.ViewportToWorldPoint(new Vector3(-0.01f, 0, Camera.main.nearClipPlane));
-            muroIzquierdo.transform.position = new Vector3(pos.x, pos.y, 0);
-            muroIzquierdo.transform.localScale = new Vector3(-0.1f, 50, 3);
+        pos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0f, Camera.main.nearClipPlane));
+        Recolector.transform.position = new Vector3(pos.x, pos.y, 0);
+        Recolector.transform.localScale = new Vector3(50, 0.2f, 0.2f);
 
-            GameObject muroDerecho = Instantiate(muro);
-            pos = Camera.main.ViewportToWorldPoint(new Vector3(1.01f, 0, Camera.main.nearClipPlane));
-            muroDerecho.transform.position = new Vector3(pos.x, pos.y, 0);
-            muroDerecho.transform.localScale = new Vector3(-0.1f, 50, 3);         
+        GameObject muroIzquierdo = Instantiate(muro);
+        pos = Camera.main.ViewportToWorldPoint(new Vector3(-0.01f, 0, Camera.main.nearClipPlane));
+        muroIzquierdo.transform.position = new Vector3(pos.x, pos.y, 0);
+        muroIzquierdo.transform.localScale = new Vector3(-0.1f, 50, 3);
 
-            GameObject muroArriba = Instantiate(muro);
-            pos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1, Camera.main.nearClipPlane));
-            muroArriba.transform.position = new Vector3(pos.x, pos.y, 0);
-            muroArriba.transform.localScale = new Vector3(50, 2f, 3);                
+        GameObject muroDerecho = Instantiate(muro);
+        pos = Camera.main.ViewportToWorldPoint(new Vector3(1.01f, 0, Camera.main.nearClipPlane));
+        muroDerecho.transform.position = new Vector3(pos.x, pos.y, 0);
+        muroDerecho.transform.localScale = new Vector3(-0.1f, 50, 3);
+
+        GameObject muroArriba = Instantiate(muro);
+        pos = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 1, Camera.main.nearClipPlane));
+        muroArriba.transform.position = new Vector3(pos.x, pos.y, 0);
+        muroArriba.transform.localScale = new Vector3(50, 2f, 3);
     }
 
-    void WriteString()
+    void CrearBloque(int x, int y, int tipo, int golpes)
+    {
+        GameObject nuevoBloque = Instantiate(bloque);
+
+        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(x * distanciaX, 1 - (y * distanciaY), Camera.main.nearClipPlane));
+        nuevoBloque.transform.position = new Vector3(pos.x, pos.y, 0);
+
+        //nuevoBloque.transform.position = new Vector3(x, y, 0);
+        nuevoBloque.GetComponent<ComportamientoBloque>().contGolpes = golpes;
+    }
+
+    public void ReadLevel(string nivel)
+    {
+        string path = "Assets/Levels/" + nivel;
+
+        //Read the text from directly from the test.txt file
+        StreamReader archivo = new StreamReader(path);
+        int j = 0;
+        int contPuntos = 0;
+
+        List<infoBloque> infoBloques = new List<infoBloque>(); //Almacenamos info primera vuelta, en la segunda, creamos los bloques  
+
+        while (archivo != null)
+        {
+            string fila = archivo.ReadLine();
+
+            //Layer 1
+            if (j >= 3 && j < 14)
+            {
+                for (int i = 0; i < fila.Length; i++)
+                {
+                    if (fila[i] == '.')
+                        contPuntos++;
+
+                    //Si existe bloque
+                    if (fila[i] != ',' && contPuntos < 1 && fila[i] != '0' && fila[i] != '.')
+                    {
+                        //CrearBloque(i, j, (int)System.Char.GetNumericValue(fila[i]));
+                        infoBloque aux;
+                        aux.x = i; aux.y = j; aux.tipo = (int)System.Char.GetNumericValue(fila[i]);
+                        infoBloques.Add(aux);
+                    }
+
+                }//Fin del for
+            }
+
+            //Layer 2
+            if (j > 16)
+            {
+                for (int i = 0; i < fila.Length; i++)
+                {
+                    if (fila[i] == '.')
+                        contPuntos++;
+
+                    //Matriz que da valor a los bloques, creacion
+                    if (fila[i] != ',' && contPuntos >= 1 && fila[i] != '0' && fila[i] != '.')
+                    {
+                        CrearBloque(infoBloques[i].x, infoBloques[i].y, infoBloques[i].tipo, (int)System.Char.GetNumericValue(fila[i]));
+                    }
+
+                }//Fin del for
+            }
+
+            j++;
+
+            if (contPuntos > 1) //Final del archivo
+                break;
+        }
+
+    }
+
+    void WriteTxt()
     {
         string path = "Assets/Resources/test.txt";
 
@@ -68,48 +149,4 @@ public class GameManager : MonoBehaviour {
         Debug.Log(asset.text);
     }
 
-    void CrearBloque(int x, int y, int golpes)
-    {
-        GameObject nuevoBloque = Instantiate(bloque);
-
-        Vector3 pos = Camera.main.ViewportToWorldPoint(new Vector3(x*distanciaX, 1 - (y*distanciaY), Camera.main.nearClipPlane));
-        nuevoBloque.transform.position = new Vector3(pos.x, pos.y, 0);
-
-
-        //nuevoBloque.transform.position = new Vector3(x, y, 0);
-        nuevoBloque.GetComponent<ComportamientoBloque>().contGolpes = golpes; 
-    }
-
-    
-    public void ReadLevel(string nivel)
-    {
-        string path = "Assets/Levels/" + nivel;
-
-        //Read the text from directly from the test.txt file
-        StreamReader archivo = new StreamReader(path);
-        int j = 0;
-
-        while (archivo != null)
-        {
-            string fila = archivo.ReadLine();
-            if (fila.Length > 0 && j >= 3)
-            {
-                for (int i = 0; i < fila.Length; i++)
-                {
-                    //Si existe bloque
-                    if ((int)System.Char.GetNumericValue(fila[i]) > 0)
-                    {
-                        CrearBloque(i, j, (int)System.Char.GetNumericValue(fila[i]));
-                    }
-                }
-            }
-            j++;            
-        }
-        
-    }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
